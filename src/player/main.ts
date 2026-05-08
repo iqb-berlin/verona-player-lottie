ab66f5ab31dca8a877996f47f279b09d23ac53f5
irathgen <rathgent@iqb.hu-berlin.de>
2026-05-08 09:46:41 +0200

    add inline dotlottie wasm

--- src/player/main.ts
+++ src/player/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { DotLottie } from '@lottiefiles/dotlottie-web';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+DotLottie.setWasmUrl('assets/dotlottie-player.wasm');
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
